85287d42cc9fe242cbb5807f1ac709fc0f4bc381
Domenic-Cianfichi <djcian14@gmail.com>
2015-03-31 21:04:35 -0700

    updating getSquare to getRect
    getSquare is now getRect and takes in a height and a width instead of an area.

--- app/src/main/java/groovycarnage/com/hermes/utility/SphericalUtilFunctions.java
+++ app/src/main/java/groovycarnage/com/hermes/utility/SphericalUtilFunctions.java
@@ -16,14 +16,22 @@ public class SphericalUtilFunctions {
         return distance;
     }
 
-    // returns the top right and bottom left corners for a square of size "area" centered at "center"
+    // returns the top right and bottom left corners for a rectangle with a certain height and width centered at "center"
     // return value is stored as an array of LatLng {topRight, bottomLeft}
-    // assumes "area" is the area in square miles.
-    public static LatLng[] getSquare(double area, LatLng center) {
-        area *= 1137.97806; // convert to meters, divided by 2, multiplied by sqrt(2) to get distance from corner to center in meters
+    // assumes "height" and "width" are the height and width of the rectangle in miles
+    public static LatLng[] getRect(double height, double width, LatLng center) {
 
-        LatLng x = SphericalUtil.computeOffset(center, area, 45); // top right
-        LatLng y = SphericalUtil.computeOffset(center, area, 225); // bottom left
+        height *= 804.672; // convert to meters, divided by 2, to get distance from corner to side in meters
+        width *= 804.672; // convert to meters, divided by 2, to get distance from corner to side in meters
+
+        LatLng x1 = SphericalUtil.computeOffset(center, height, 0); // top
+        LatLng x2 = SphericalUtil.computeOffset(center, height, 180); // bottom
+        LatLng y1 = SphericalUtil.computeOffset(center, width, 270); // left
+        LatLng y2 = SphericalUtil.computeOffset(center, width, 90); // right
+
+        LatLng x = new LatLng(x1.latitude, y2.longitude); // top right
+        LatLng y = new LatLng(x2.latitude, y1.longitude); // bottom left
+
         LatLng corners[] = {x, y};
         return (corners);
     }
